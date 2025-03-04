3fe6885647c79be4a25d320cd0b45076d76b648c
satyPunk <satyamhere8@gmail.com>
2025-03-04 21:17:14 +0530

    Stage-6

---
+++ components/ui/customForm.tsx
@@ -0,0 +1,35 @@
+
+use 'client'
+import { Input } from "@/components/ui/input"
+
+import React from 'react';
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+  } from "@/components/ui/form"
+const customForm =() =>{
+    return (
+        <FormField
+                  control={form.control}
+                  name="username"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Username</FormLabel>
+                      <FormControl>
+                        <Input placeholder="shadcn" {...field} />
+                      </FormControl>
+                      <FormDescription>
+                        This is your public display name.
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+    );
+}
+export default customForm;
