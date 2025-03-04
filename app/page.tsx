245baf8cc4e24f7e18eeadcbb75877896fbe3649
satyPunk <satyamhere8@gmail.com>
2025-03-04 20:52:47 +0530

    Stage-5

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,35 @@
 import Image from "next/image";
 import { Button }from "@/components/ui/button";
+import Link from "next/link";
+import Patientform from "@/components/forms/Patientform"
 export default function Home() {
-  return (<div className ="flex j-screen max-h-screen">
+  return (<div className ="flex h-screen max-h-screen">
     <section className ="remove-scrollbar container my-auto">
       <div className="sub-container max-w-[496px]">
-        {/* <Image 
-        
-        /> */}
-        <h1 className ="text-white text-3xl underline">HEY guys</h1>
+        <Image 
+        src= "/assets/icons/logo-full.svg"
+        height={1000}
+        width ={1000}
+        alt ="patient"
+        className="mb-12 h-10 w-fit"
+        />
+        <Patientform />
+        <div className="text-14-regular">
+          <p className ="justify-items-end text-dark-600 xl:text-left">© 2025 NeoCare</p>
+          <Link href="/?admin=true" className="text-green-500">
+            Admin
+          </Link>
+        </div>
       </div>
     </section>
+    <Image 
+  src="/assets/images/onboarding-img.png"
+  height={1000}
+  width={1000}
+  alt="patient"
+  className="side-img w-full sm:max-w-[75%] md:max-w-[50%]"
+/>
+
     </div>
   );
 }
