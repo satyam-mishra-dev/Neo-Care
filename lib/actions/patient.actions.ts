db61cfc710d034c4248c68b11e089f08994db170
satyPunk <satyamhere8@gmail.com>
2025-03-16 15:14:28 +0530

    Stage-33

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,7 +1,7 @@
 "use server"; // Ensures this runs only on the server
 
-import { Query, ID } from "node-appwrite";
-import { users } from "../appwrite.config";
+import { Query, ID, InputFile } from "node-appwrite";
+import { BUCKET_ID, DATABASE_ID, databases, ENDPOINT, PATIENT_COLLECTION_ID, PROJECT_ID, storage, users } from "../appwrite.config";
 import { parseStringify } from "../utils"; // Ensure this type exists
 
 export const createUser = async (user: CreateUserParams) => {
@@ -27,6 +27,22 @@ export const getUser = async (userId:string)=>{
 }
 export const registerUser = async ({identificationDocument,...patient}:RegisterUserParams)=>{
     try{
+        let file;
+        if (identificationDocument) {
+            const inputFile = InputFile.fromBuffer(
+                identificationDocument?.get('blobFile') as Blob,
+                identificationDocument?.get('fileName') as string,
+            )
+            file = await storage.createFile(BUCKET_ID!,ID.unique(),inputFile); 
+        }
+        const newawait databases.createDocument(
+            DATABASE_ID,PATIENT_COLLECTION_ID,ID.unique(),{
+                identificationDocumentId:file?.$id || null,
+                identificationDocumentUrl:`${ENDPOINT}/storage/buckets/${BUCKET_ID}/files/${file?.$id}/view?project=${PROJECT_ID}`,
+                ...patient
+            }
+        )
+        return parseStringify(patient);
 
     }catch(err){
         console.log(err)
