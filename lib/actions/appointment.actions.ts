897353d0d0d7bbac2425f6922176c6f25e445eea
satyPunk <satyamhere8@gmail.com>
2025-03-24 00:51:46 +0530

    Stage-57

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -6,8 +6,8 @@ import { parseStringify } from "../utils";
 export const CreateAppointment = async (appointment: CreateAppointmentParams) => {
     try{
         const newAppointment = await databases.createDocument(
-            DATABASE_ID,
-            APPOINTMENT_COLLECTION_ID,
+            DATABASE_ID!,
+            APPOINTMENT_COLLECTION_ID!,
             ID.unique(),
             appointment
         )
