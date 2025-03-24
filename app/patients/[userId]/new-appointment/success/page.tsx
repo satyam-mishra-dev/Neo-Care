71e88bb3cb99b55054f579bb52201369b77ffe23
satyPunk <satyamhere8@gmail.com>
2025-03-24 19:51:42 +0530

    Stage-59

--- app/patients/[userId]/new-appointment/success/page.tsx
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -4,8 +4,8 @@ import  SuccessIcon  from '@/components/ui/checkCircle';
 const Success = () => {
   return (
     <>
-      <div className="flex h-screen items-start justify-center pt-8"> {/* ✅ Adjusted alignment */}
-        <div className="flex flex-col items-center gap-4">
+        <section className="flex flex-col items-center gap-4">
+          <div className='px-10 py-10 mt-10'>
           <Image
           src="/assets/icons/logo-full.svg"
           height={50}
@@ -13,12 +13,17 @@ const Success = () => {
           alt="logo"
           className="h-10 w-fit "
         />
-        <div className='mt-16'>
-        <SuccessIcon />
         </div>
-        </div>
+        <Image
+        src="/assets/gifs/success.gif"
+        height={200}
+        width={200}
+        alt="success"
+        />
+        <h2 className='header mt-10 mb-6 max-w-[600px] text-center mx-15'>Your <span className='text-green-500'>appointment request </span>has been successfully submitted!!</h2>
+        <p className='text-center'>We will be in touch shortly to confirm the appointment</p>
+        </section>
     
-      </div>
     </>
   );
 };
