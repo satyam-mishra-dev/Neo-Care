7b798b9bee63e2e9d6c3c6c7f6ef70de264e2966
satyPunk <satyamhere8@gmail.com>
2025-03-24 19:15:54 +0530

    Stage-58

--- app/patients/[userId]/new-appointment/success/page.tsx
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -5,14 +5,18 @@ const Success = () => {
   return (
     <>
       <div className="flex h-screen items-start justify-center pt-8"> {/* ✅ Adjusted alignment */}
-        <Image
+        <div className="flex flex-col items-center gap-4">
+          <Image
           src="/assets/icons/logo-full.svg"
           height={50}
           width={50}
           alt="logo"
-          className="h-10 w-fit"
+          className="h-10 w-fit "
         />
+        <div className='mt-16'>
         <SuccessIcon />
+        </div>
+        </div>
     
       </div>
     </>
--- components/ui/checkCircle.tsx
+++ components/ui/checkCircle.tsx
@@ -2,9 +2,7 @@ import { CheckCircle } from "lucide-react";
 
 const SuccessIcon = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen">
       <CheckCircle className="text-green-500" size={100} />
-    </div>
   );
 };
 
